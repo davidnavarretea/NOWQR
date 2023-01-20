d17d0129d1b9ea3da46ca506f230872dfe1c46aa
David <davidnavarretea1996@gmail.com>
2023-01-20 09:09:18 +0100

    ya descarga la veces que se le pide en google chrome y edita la data del qrCode pero descarga siempre la mesa con el número del final del bucle

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import QRCodeStyling from "qr-code-styling";
+import { useEffect, useState } from "react";
 function App() {
-  const qrCode = new QRCodeStyling({
-    width: 3000,
-    height: 3000,
-    type: "png",
-    data: "https://now.tienda/",
-    image: "nowtienda.png",
-    dotsOptions: {
+  const [datos, setDatos] = useState("https://now.tienda/");
+  useEffect(() => {
+    const qrCode = new QRCodeStyling({
+      width: 3000,
+      height: 3000,
+      type: "png",
+      data: datos,
+      image: "nowtienda.png",
+      dotsOptions: {
         color: "#000",
-        type: "dots"
-    },
-    imageOptions: {
+        type: "dots",
+      },
+      imageOptions: {
         crossOrigin: "anonymous",
-        margin: 20
-    },
-    cornersSquareOptions: {
-      type: "dot"
-    },
-    cornersDotOptions: {
-      type: "dot"
-    },
-  });
+        margin: 20,
+      },
+      cornersSquareOptions: {
+        type: "dot",
+      },
+      cornersDotOptions: {
+        type: "dot",
+      },
+    });
 
-  qrCode.append(document.getElementById("canvas"));
-  qrCode.download({ name: "qr", extension: "svg" });
-  return (
-<div id="canvas"></div>
-  )
+    qrCode.append(document.getElementById("canvas"));
+
+    for (let i = 1; i <= 10; i++) {
+      setDatos(`https://demo.now.tienda/mesa${i}`);
+      qrCode.download({ name: `Mesa ${i}`, extension: "svg" });
+    }
+  }, [datos]);
+
+  return <div id="canvas"></div>;
 }
 
 export default App;
